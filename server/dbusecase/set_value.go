d0b9bdad20c0893569170baebde1ba3c39296727
SSripilaipong <SHSnail@mail.com>
2023-05-27 16:53:20 +0700

    feat(server.dbusecase.setValue): Test_should_send_key_to_storage
    Refs: #3

--- server/dbusecase/set_value.go
+++ server/dbusecase/set_value.go
@@ -16,16 +16,18 @@ type SetValueResponse struct {
 type SetValueFunc func(context.Context, *SetValueRequest) (*SetValueResponse, error)
 
 func SetValueUsecase(dep *Dependency) SetValueFunc {
-	return func(context.Context, *SetValueRequest) (*SetValueResponse, error) {
-		dep.storageChan <- setValueMessage{}
+	return func(ctx context.Context, request *SetValueRequest) (*SetValueResponse, error) {
+		dep.storageChan <- setValueMessage{key: request.Key}
 		return nil, nil
 	}
 }
 
-type setValueMessage struct{}
+type setValueMessage struct {
+	key string
+}
 
 func (m setValueMessage) Key() string {
-	return ""
+	return m.key
 }
 
 func (m setValueMessage) Value() string {
--- tests/server/dbusecase/setValue/call_storage_test.go
+++ tests/server/dbusecase/setValue/call_storage_test.go
@@ -26,3 +26,20 @@ func Test_should_send_set_value_message_to_storage(t *testing.T) {
 	_, isSetValueMessage := message.(storage.SetValueMessage)
 	assert.True(t, isSetValueMessage)
 }
+
+func Test_should_send_key_to_storage(t *testing.T) {
+	storageChan := make(chan any, 2)
+	execute := dbusecase.SetValueUsecase(dbusecase.NewDependency(storageChan))
+
+	go func() {
+		_, _ = execute(context.Background(), &dbusecase.SetValueRequest{Key: "Go", Value: "Lang"})
+	}()
+
+	message, ok := goutil.ReceiveWithTimeout(storageChan, defaultTimeout)
+	if !ok {
+		panic(fmt.Errorf("unexpected error"))
+	}
+
+	parsedMessage, _ := message.(storage.SetValueMessage)
+	assert.Equal(t, "Go", parsedMessage.Key())
+}
