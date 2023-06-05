7f7be7893507f3fc299c74e155426491b682d5ab
SSripilaipong <SHSnail@mail.com>
2023-06-05 18:15:21 +0700

    refactor(server.dbstorage): move creation logic of repository inside storagerepository
    Refs: #8

--- server/dbstorage/main.go
+++ server/dbstorage/main.go
@@ -2,11 +2,10 @@ package dbstorage
 
 import (
 	"go-skv/server/dbstorage/storagerecordfactory"
-	"go-skv/server/dbstorage/storagerepository/repositoryroutine"
+	"go-skv/server/dbstorage/storagerepository"
 )
 
-func New(storageBufferSize int, recordBufferSize int) (Repository, chan<- any) {
-	ch := make(chan any, storageBufferSize)
-	s := repositoryroutine.New(ch, storagerecordfactory.NewFactory(recordBufferSize))
+func New(storageBufferSize int, recordBufferSize int) (Repository, chan<- any) { // TODO: return interactor instead of channel
+	s, _, ch := storagerepository.New(storageBufferSize, storagerecordfactory.NewFactory(recordBufferSize))
 	return s, ch
 }
---
+++ server/dbstorage/storagerepository/type.go
@@ -0,0 +1,9 @@
+package storagerepository
+
+import (
+	"go-skv/server/dbstorage/storagerepository/repositoryinteractor"
+	"go-skv/server/dbstorage/storagerepository/repositoryroutine"
+)
+
+type Routine = repositoryroutine.Interface
+type Interactor = repositoryinteractor.Interface
