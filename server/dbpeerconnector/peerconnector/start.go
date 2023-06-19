651e558b841ff7e916b52ca1a4a23424edffe429
SSripilaipong <SHSnail@mail.com>
2023-06-19 17:17:11 +0700

    feat(server.dbpeerconnector.peerconnector.start): Test_should_not_connect_to_next_peer_if_the_first_peer_can_be_connected
    Refs: #12

--- server/dbpeerconnector/peerconnector/start.go
+++ server/dbpeerconnector/peerconnector/start.go
@@ -3,15 +3,26 @@ package peerconnector
 import (
 	"errors"
 	"go-skv/server/dbpeerconnector/peerclient/peerclientcontract"
+	"go-skv/server/dbpeerconnector/peerconnectorcontract"
 	"go-skv/util/goutil"
 )
 
 func (p connector) Start() error {
+	_ = p.connectToExistingPeer()
+	return nil
+}
+
+func (p connector) connectToExistingPeer() peerconnectorcontract.Peer {
+	var peer peerconnectorcontract.Peer
+	var err error
 	for _, addr := range p.existingPeerAddresses {
-		_, err := p.client.ConnectToPeer(addr)
+		peer, err = p.client.ConnectToPeer(addr)
+		if err == nil {
+			break
+		}
 		if !errors.Is(err, peerclientcontract.ConnectionError{}) {
 			goutil.PanicUnhandledError(err)
 		}
 	}
-	return nil
+	return peer
 }
--- tests/server/dbpeerconnector/peerconnector/start/main_test.go
+++ tests/server/dbpeerconnector/peerconnector/start/main_test.go
@@ -34,3 +34,15 @@ func Test_should_connect_to_next_peer_if_the_first_peer_cannot_be_connected(t *t
 	goutil.PanicUnhandledError(connector.Stop())
 	assert.Equal(t, []string{"1.1.1.1:1111", "2.2.2.2:2222"}, client.ConnectToPeer_address_array)
 }
+
+func Test_should_not_connect_to_next_peer_if_the_first_peer_can_be_connected(t *testing.T) {
+	client := &peerconnectortest.PeerClientMock{
+		ConnectToPeer_Return_array: []peerconnectorcontract.Peer{&peerconnectortest.PeerMock{}},
+	}
+	connector := peerconnector.New([]string{"1.1.1.1:1111", "2.2.2.2:2222"}, client)
+
+	goutil.PanicUnhandledError(connector.Start())
+
+	goutil.PanicUnhandledError(connector.Stop())
+	assert.Equal(t, []string{"1.1.1.1:1111"}, client.ConnectToPeer_address_array)
+}
